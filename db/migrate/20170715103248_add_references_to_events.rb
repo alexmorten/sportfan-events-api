b7f873a429c7aa834271254f2ee3ddfd6fcfa184
alexmorten <alexandrius.martinus@gmx.de>
2017-07-28 00:20:49 +0200

    changed id to be a uuid

--- db/migrate/20170715103248_add_references_to_events.rb
+++ db/migrate/20170715103248_add_references_to_events.rb
@@ -1,6 +1,6 @@
 class AddReferencesToEvents < ActiveRecord::Migration[5.0]
   def change
-    add_reference :events, :user,index: true
-    add_reference :events, :group,index: true
+    add_reference :events, :user,index: true, type: :uuid
+    add_reference :events, :group,index: true, type: :uuid
   end
 end
